f68d9eface390bcfd6edefa561be77b41156d1e4
VASHIKA TYAGI <137616546+vashika11@users.noreply.github.com>
2025-08-17 23:25:57 +0530

    healthsync.py

--- healthsync.py
+++ healthsync.py
@@ -1,17 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to HealthSync!"
+    return "HealthSync is running!"
 
-# Add more routes below as needed
-# Example:
-# @app.route('/api/data', methods=['POST'])
-# def receive_data():
-#     data = request.get_json()
-#     return jsonify({"status": "received", "data": data})
-
-if __name__ == '__main__':
-    app.run(debug=True)
